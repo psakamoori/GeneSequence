8e928c8e34a552f91ac315803a75d3f2bac67bfc
Pradeep Sakhamoori <pradeep.sakhamoori@intel.com>
2017-05-31 11:12:27 -0700

    gene.py: Adding coding comments

--- neon/neon/data/gene.py
+++ neon/neon/data/gene.py
@@ -4,14 +4,20 @@ import numpy as np
 def load_data(filepath, filename, names, validation_size):
 
     filepath = filepath + '/' + filename
+
+    # Reading CSV file
     dataset = pd.read_csv(filepath, names=names)
 
     arr = dataset.values
 
+    # Extracting Characters of DNA sequence
     data = arr[:,1:8]
+
+    # Extracting label (or) class type EI/IF/N
     labels = arr[:,0]
     label = []
 
+    # Based on class type, converting str labesl to int lables 
     for cl in labels:
         if cl == str('ei'):
             label.append(int(0))
@@ -21,11 +27,14 @@ def load_data(filepath, filename, names, validation_size):
             label.append(int(2))
     seed = 7
 
+    # Extracing 80% of 3190 splice dataset values for training i.e 2552
+    # Getting 20% of 3190 splice dataset values for validation i.e 638
     train_data = np.zeros((2552, 4))
     valid_data = np.zeros((638, 4))
     train_label = np.zeros((2552,1))
     valid_label = np.zeros((638, 1))
-    
+   
+    # Using sklearn implemenation to split the train and validation datasets
     train_data, valid_data, train_label, valid_label = model_selection.train_test_split(data, label, test_size=validation_size, random_state=seed)
 
     t_label = np.zeros((2552, 1))
